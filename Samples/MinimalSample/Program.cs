78dda329f18e8df76593027158d4dc872bffd65b
Jan Lukas Gernert <janlukas.gernert@metrilus.de>
2017-10-18 10:41:26 +0200

    Revert changes to unrelated files

--- Samples/MinimalSample/Program.cs
+++ Samples/MinimalSample/Program.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using MetriPrimitives.Data;
 
 namespace MetriCam2.Samples.MinimalSample
 {
@@ -22,10 +21,10 @@ namespace MetriCam2.Samples.MinimalSample
             Console.WriteLine("------------------------------------------");
 
             // Create camera object
-            RealSense2 camera;
+            Camera camera;
             try
             {
-                camera = new RealSense2();
+                camera = new Kinect2();
             }
             catch (Exception e)
             {
@@ -40,28 +39,14 @@ namespace MetriCam2.Samples.MinimalSample
             Console.WriteLine("Connecting camera");
             camera.Connect();
 
-            camera.LoadConfigPreset(AdvancedMode.Preset.SHORT_RANGE);
-            camera.DeactivateChannel(ChannelNames.ZImage);
-            camera.ActivateChannel(RealSense2.CustomChannelNames.Left);
-            camera.ActivateChannel(RealSense2.CustomChannelNames.Right);
-
             Console.WriteLine("Fetching one frame");
-
-            for(int i = 0; i < 15; i++)
-            {
-                camera.Update();
-            }
-
-
-            ProjectiveTransformationZhang proj = (ProjectiveTransformationZhang)camera.GetIntrinsics(RealSense2.CustomChannelNames.Left);
-            RigidBodyTransformation rbt = camera.GetExtrinsics(RealSense2.CustomChannelNames.Left, ChannelNames.Color);
+            camera.Update();
 
             try
             {
                 Console.WriteLine("Accessing color data");
                 ColorCameraImage img = (ColorCameraImage)camera.CalcChannel(ChannelNames.Color);
                 Bitmap rgbBitmapData = img.ToBitmap();
-                rgbBitmapData.Save("0Color.bmp");
             }
             catch (ArgumentException ex)
             {
@@ -70,34 +55,8 @@ namespace MetriCam2.Samples.MinimalSample
 
             try
             {
-                Console.WriteLine("Accessing left IR data");
-                FloatCameraImage img = (FloatCameraImage)camera.CalcChannel(RealSense2.CustomChannelNames.Left);
-                FloatImage fimg = new FloatImage(ref img);
-                fimg.Save("left.flt");
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(String.Format("Error getting channel {0}: {1}.", RealSense2.CustomChannelNames.Left, ex.Message));
-            }
-
-            try
-            {
-                Console.WriteLine("Accessing left IR data");
-                FloatCameraImage img = (FloatCameraImage)camera.CalcChannel(RealSense2.CustomChannelNames.Right);
-                FloatImage fimg = new FloatImage(ref img);
-                fimg.Save("right.flt");
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(String.Format("Error getting channel {0}: {1}.", RealSense2.CustomChannelNames.Right, ex.Message));
-            }
-
-            try
-            {
                 Console.WriteLine("Accessing distance data");
-                FloatCameraImage distancesData = (FloatCameraImage)camera.CalcChannel(ChannelNames.ZImage);
-                FloatImage fimg = new FloatImage(ref distancesData);
-                fimg.Save("depth.flt");
+                FloatCameraImage distancesData = (FloatCameraImage)camera.CalcChannel(ChannelNames.Distance);
             }
             catch (ArgumentException ex)
             {
