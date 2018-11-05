a16cee43ce49bdaee437893e3c4fef94926a5b52
Hannes <582746+f00f@users.noreply.github.com>
2018-11-05 16:02:50 +0100

    Improvements to endianess conversion

--- BetaCameras/Sick.VisionaryT/Utils.cs
+++ BetaCameras/Sick.VisionaryT/Utils.cs
@@ -15,29 +15,11 @@ namespace MetriCam2.Cameras.Internal.Sick
     internal class Utils
     {
         /// <summary>
-        /// Converts an big endian uint32_t and converts it to little if the system is little endian.
-        /// </summary>
-        /// <param name="value">big endian uint32_t</param>
-        /// <returns>corresponding little endian uint32_t</returns>
-        internal static UInt32 ConvertEndiannessUInt32(UInt32 value)
-        {
-            if (!BitConverter.IsLittleEndian)
-            {
-                return value;
-            }
-
-            byte[] bytes = BitConverter.GetBytes(value);
-            Array.Reverse(bytes);
-
-            return BitConverter.ToUInt32(bytes, 0);
-        }
-
-        /// <summary>
         /// Converts an big endian uint16_t and converts it to little if the system is little endian.
         /// </summary>
         /// <param name="value">big endian uint16_t</param>
-        /// <returns>corresponding little endian uint16_t</returns>
-        internal static UInt16 ConvertEndiannessUInt16(UInt16 value)
+        /// <returns>system-uint16_t</returns>
+        private static UInt16 FromBigEndianUInt16(UInt16 value)
         {
             if (!BitConverter.IsLittleEndian)
             {
@@ -50,12 +32,65 @@ namespace MetriCam2.Cameras.Internal.Sick
             return BitConverter.ToUInt16(bytes, 0);
         }
 
+        internal static UInt16 FromBigEndianUInt16(byte[] buffer, int offset)
+        {
+            UInt16 val = BitConverter.ToUInt16(buffer, offset);
+            return FromBigEndianUInt16(val);
+        }
+
+        /// <summary>
+        /// Converts an big endian uint32_t and converts it to little if the system is little endian.
+        /// </summary>
+        /// <param name="value">big endian uint32_t</param>
+        /// <returns>system-endian uint32_t</returns>
+        private static UInt32 FromBigEndianUInt32(UInt32 value)
+        {
+            if (!BitConverter.IsLittleEndian)
+            {
+                return value;
+            }
+
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        internal static UInt32 FromBigEndianUInt32(byte[] buffer, int offset)
+        {
+            UInt32 val = BitConverter.ToUInt32(buffer, offset);
+            return FromBigEndianUInt32(val);
+        }
+
+        /// <summary>
+        /// Converts an big endian int32_t and converts it to little if the system is little endian.
+        /// </summary>
+        /// <param name="value">big endian int32_t</param>
+        /// <returns>system-endian int32_t</returns>
+        private static Int32 FromBigEndianInt32(Int32 value)
+        {
+            if (!BitConverter.IsLittleEndian)
+            {
+                return value;
+            }
+
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        internal static Int32 FromBigEndianInt32(byte[] buffer, int offset)
+        {
+            Int32 val = BitConverter.ToInt32(buffer, offset);
+            return FromBigEndianInt32(val);
+        }
+
         /// <summary>
         /// Converts an big endian uint64_t and converts it to little if the system is little endian.
         /// </summary>
         /// <param name="value">big endian uint64_t</param>
-        /// <returns>corresponding little endian uint64_t</returns>
-        internal static UInt64 ConvertEndiannessUInt64(UInt64 value)
+        /// <returns>system-endian uint64_t</returns>
+        private static UInt64 FromBigEndianUInt64(UInt64 value)
         {
             if (!BitConverter.IsLittleEndian)
             {
@@ -66,6 +101,43 @@ namespace MetriCam2.Cameras.Internal.Sick
             Array.Reverse(bytes);
 
             return BitConverter.ToUInt64(bytes, 0);
+        }
+
+
+        internal static UInt64 FromBigEndianUInt64(byte[] buffer, int offset)
+        {
+            UInt64 val = BitConverter.ToUInt64(buffer, offset);
+            return FromBigEndianUInt64(val);
+        }
+
+        /// <summary>
+        /// Converts a uint32_t to big endian.
+        /// </summary>
+        /// <param name="value">system-endian uint32_t</param>
+        /// <returns>big endian uint32_t</returns>
+        internal static byte[] ToBigEndian(uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+
+        /// <summary>
+        /// Converts a int32_t to big endian.
+        /// </summary>
+        /// <param name="value">system-endian int32_t</param>
+        /// <returns>big endian int32_t</returns>
+        internal static byte[] ToBigEndian(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
         }
 
         /// <summary>
