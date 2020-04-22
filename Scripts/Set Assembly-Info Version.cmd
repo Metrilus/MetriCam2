63db2f753e692d19ca995ed66ae19c220fe65a6c
sisiplac <simon.placht@metrilus.de>
2020-04-22 11:36:40 +0200

    Fix wrong assembly version attribute for C++/CLI projects.

--- Scripts/Set Assembly-Info Version.cmd
+++ Scripts/Set Assembly-Info Version.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 echo Updating Assembly-Info: %2
-@powershell -Command "(get-content \"%~f1\") | foreach-object { $_ -replace \"(Assembly\w*Version)\(`\"(\d+\.\d+\.\d+\.\d+)`\"\)\", \"`$1(`\"%2`\")\" } | set-content \"%~f1\""
+@powershell -Command "(get-content \"%~f1\") | foreach-object { $_ -replace \"(Assembly\w*Version\w*)\(`\"(\d+\.\d+\.\d+\.\d+)`\"\)\", \"`$1(`\"%2`\")\" } | set-content \"%~f1\""
